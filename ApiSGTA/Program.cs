3c706bcfe816bfbecc9e7788c15496c6b4cbfc27
Isa94d-lab <181164074+Isa94d-lab@users.noreply.github.com>
2025-06-20 19:11:46 -0500

    feat: :sparkles: Swagger Connection
    Program.cs file was modified for the correct operation of swagger (removing MapOpenApi)

--- ApiSGTA/Program.cs
+++ ApiSGTA/Program.cs
@@ -1,14 +1,61 @@
 using System.Reflection;
 using Infrastructure.Data;
 using Microsoft.EntityFrameworkCore;
+using ApiSGTA.Services;
+using Infrastructure.UnitOfWork;
+using Application.Interfaces;
+// Add swager
+using Microsoft.OpenApi.Models;
+
 
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddAutoMapper(Assembly.GetEntryAssembly());
 
+
+// This method records Swagger configuration
+builder.Services.AddSwaggerGen(options =>
+{
+    // Basic information on swagger documentation
+    options.SwaggerDoc("v1", new OpenApiInfo
+    {
+        Title = "SGTA API",
+        Version = "v1"
+    });
+
+    // Here we define how Swagger will request the JWT token
+    options.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
+    {
+        Name = "Authorization",
+        In = ParameterLocation.Header,
+        Type = SecuritySchemeType.ApiKey,
+        Scheme = "Bearer",
+        BearerFormat = "JWT",
+        Description = "Escribe: Bearer {tu token JWT aquí}"
+    });
+
+    // Here it is shown that swagger will know that all endpoints will use JWT and will apply the token sent with “Authorize”
+    options.AddSecurityRequirement(new OpenApiSecurityRequirement
+    {
+        {
+            new OpenApiSecurityScheme
+            {
+                Reference = new OpenApiReference
+                {
+                    Id = "Bearer",
+                    Type = ReferenceType.SecurityScheme
+                }
+            },
+            Array.Empty<string>()
+        }
+    });
+});
+
+
+
 // Add services to the container.
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
-builder.Services.AddOpenApi();
+// builder.Services.AddOpenApi();
 
 builder.Services.AddDbContext<AutoTallerDbContext>(options =>
 {
@@ -17,13 +64,39 @@ builder.Services.AddDbContext<AutoTallerDbContext>(options =>
     //options.UseQueryTrackingBehavior(QueryTrackingBehavior.NoTracking);
 });
 
+
+// Servicios de aplicación
+builder.Services.AddScoped<IUserService, UserService>();
+builder.Services.AddScoped<IUnitOfWork, UnitOfWork>();
+
+
+
+
+
+
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
+
+
+// Checks if the application is running in the environment
 if (app.Environment.IsDevelopment())
 {
-    app.MapOpenApi();
+    // Starts JSON generation of Swagger 
+    app.UseSwagger();
+    app.UseSwaggerUI(options =>
+    {
+        options.SwaggerEndpoint("/swagger/v1/swagger.json", "SGTA API V1");
+        options.RoutePrefix = string.Empty; // To load the root, as an example: “https://localhost:5000/”
+    });
 }
+
+// Necessary to use JWT
+// First
+app.UseAuthentication();
+// Second
+app.UseAuthorization();
+
+
 
 app.UseHttpsRedirection();
 
