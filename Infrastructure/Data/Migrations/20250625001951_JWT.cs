7604f2ac1557367a01984f7a1602560bc5209cb0
Laura <181015028+LauraVargas22@users.noreply.github.com.>
2025-06-24 19:25:11 -0500

    feat: :sparkles: New migration and fix bug in AutoTallerDbContext

---
+++ Infrastructure/Data/Migrations/20250625001951_JWT.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Infrastructure.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class JWT : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "updatedAt",
+                table: "roles",
+                newName: "updated_at");
+
+            migrationBuilder.RenameColumn(
+                name: "createdAt",
+                table: "roles",
+                newName: "created_at");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "updated_at",
+                table: "roles",
+                newName: "updatedAt");
+
+            migrationBuilder.RenameColumn(
+                name: "created_at",
+                table: "roles",
+                newName: "createdAt");
+        }
+    }
+}
