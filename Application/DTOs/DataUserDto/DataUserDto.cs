ad323060ab03dbd263b36616883e3be616490a69
Isa94d-lab <181164074+Isa94d-lab@users.noreply.github.com>
2025-06-30 15:23:27 -0500

    feat: :twisted_rightwards_arrows: Merge Branch

--- Application/DTOs/DataUserDto/DataUserDto.cs
+++ Application/DTOs/DataUserDto/DataUserDto.cs
@@ -9,13 +9,16 @@ namespace Application.DTOs
 {
     public class DataUserDto
     {
-        public int     Id        { get; set; }
+        // New Propierties added to DataUserDto
+        public int Id { get; set; }
         public string  Name      { get; set; } = "";
         public string  LastName  { get; set; } = "";
+        
         public bool    IsActive  { get; set; }
         public DateTime CreatedAt { get; set; }
         public DateTime UpdatedAt { get; set; }
-        
+
+        // New
         [JsonPropertyName("message")]
         public string? Mensaje { get; set; }
         [JsonPropertyName("isAuthenticated")]
